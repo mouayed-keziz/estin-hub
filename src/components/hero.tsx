3e911ee8197f1417cd30f5379c774615bec66862
mouayed-keziz <m_keziz@estin.dz>
2023-12-26 00:39:09 +0100

    add hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,7 +1,21 @@
+'use client'
+import { Anchor, Autocomplete, Container, Text, Title } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
+
 export default function Hero() {
     return (
         <>
-            this is the hero
+            <Container mt={100} >
+                <Title mb='md' align='center' order={1}>
+                    Welcome to Estin Hub - A Place for Non-Academic Communication, Blogs, and More
+                </Title>
+                <Text align='center' c='dimmed'>
+                    This is a place for students to communicate with each other, share their thoughts, and more.
+                    We are currently in the process of building this website, so please be patient with us.
+                    If you would like to contribute, please contact us at <Anchor href="mailto:m_keziz@estin.dz">mail</Anchor>
+                </Text>
+                <Autocomplete placeholder='Search for blogs' mt='xl' size='lg' icon={<IconSearch />} data={['1', '2', '3', '4']} />
+            </Container>
         </>
     )
 }
