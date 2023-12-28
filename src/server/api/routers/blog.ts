92e6e9382388aa8885092bf5898015ce5a92f7ff
mouayed-keziz <m_keziz@estin.dz>
2023-12-28 16:41:53 +0100

    Update blog router and schema.prisma

--- src/server/api/routers/blog.ts
+++ src/server/api/routers/blog.ts
@@ -15,9 +15,49 @@ export const blogRouter = createTRPCRouter({
       return await ctx.db.blog.findMany({});
     }),
 
+  // -----------------------------------------------------------------------------------------------
+
   get_blogs_by_role: publicProcedure
     .input(z.object({ role: z.enum(["STUDENT", "TEACHER", "CLUB"]) }))
     .query(async ({ ctx, input }) => {
-      return await ctx.db.blog.findMany({ where: { createdBy: { role: input.role } } });
+      return await ctx.db.blog.findMany({
+        where: { author: { role: { equals: input.role } } },
+        select: { title: true, image: true, author: { select: { name: true, role: true } } }
+      });
+    }),
+
+  // -----------------------------------------------------------------------------------------------
+
+  create_new_blog: protectedProcedure
+    .input(z.object({
+      title: z.string(),
+      image: z.string(),
+      content: z.string(),
+    }))
+    .query(async ({ ctx, input }) => {
+      console.log("hello worldf")
+      const new_blog = await ctx.db.blog.create({
+        data: {
+          title: input.title,
+          image: input.image,
+          content: input.content,
+          author: { connect: { id: ctx.session.user.id } }
+        }
+      })
+      return new_blog;
+    }),
+
+  // -----------------------------------------------------------------------------------------------
+
+  delete_blog: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      console.log("hell oworld")
+      const blog = await ctx.db.blog.delete({ where: { id: input.id, author: { id: ctx.session.user.id } } });
+
+      if (blog) return blog
+      else throw new Error("Failed to delete blog")
     })
+
+  // -----------------------------------------------------------------------------------------------
 });
